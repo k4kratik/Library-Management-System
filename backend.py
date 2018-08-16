14055c6e990cb12d16cd7956d9b2582dd0f17737
Kratik Jain <kratik1998@gmail.com>
2018-08-17 04:47:38 +0530

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("library.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS library (id INTEGER PRIMARY KEY, title text, author text, year integar,isbn integar)")
+    conn.commit()
+    conn.close()
+
+def insert(title,author,year,isbn):
+    conn=sqlite3.connect("library.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO library VALUES(NULL,?,?,?,?)",(title,author,year,isbn))
+    conn.commit()
+    conn.close()
+    view()
+    
+def view():
+    conn=sqlite3.connect("library.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM library")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="",author="",year="",isbn=""):
+    conn=sqlite3.connect("library.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM library  WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("library.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM library WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,year,isbn):
+    conn=sqlite3.connect("library.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE library SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+
+connect()
